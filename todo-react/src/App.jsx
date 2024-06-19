d8fd9592a81ee117c810e8c98c7236f33aaff2d3
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-06-20 01:56:58 +0300

    .

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -1,37 +1,60 @@
 import TodoList from './components/TodoList';
 import TodoCreate from './components/TodoCreate';
+import todoService from './services/todos';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
   const [todos, setTodos] = useState([]);
 
+  useEffect(() => {
+    console.log('effect')
+    todoService
+      .getAll()
+      .then(response => {
+        setTodos(response.data)
+      })
+  }, [])
+
   const createTodo = (title) => {
+
     const newTodo = { id: crypto.randomUUID(), title, completed: false };
+    todoService
+      .create(newTodo)
+      .then(res => {
+        console.log(res);
+      })
+
     const updatedTodos = [...todos, newTodo];
     setTodos(updatedTodos);
   };
 
   const removeTodo = (id) => {
-    const updatedTodos = todos.filter((todo) => todo.id !== id);
-    setTodos(updatedTodos);
+    todoService
+      .destroy(id)
+      .then(res => {
+        console.log(`Item deleted successfully, ${res.data}`);
+        const updatedTodos = todos.filter((todo) => todo.id !== id);
+        setTodos(updatedTodos);
+      })
+    
   };
 
   const changeTodo = (id, title, completed = false) => {
-    const updatedTodos = todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, title, completed };
-      }
-      return todo;
-    });
-
-    setTodos(updatedTodos);
-  };
+    const todo = todos.find(n => n.id === id);
+    const changedTodo = {...todo, title: title, completed: completed};
+    
+    todoService
+      .update(id, changedTodo)
+      .then(res => {
+        setTodos(todos.map(n => n.id !== id ? n : res.data));
+      })
+  }; 
 
   return (
     <main className="main">
       <h1>
-        React Todo <span>Streamline Your Day, the React Way!</span>
+        Daily Planner <span>Streamline Your Day, the React Way!</span>
       </h1>
       <TodoList todos={todos} removeTodo={removeTodo} changeTodo={changeTodo} />
       <TodoCreate createTodo={createTodo} />
